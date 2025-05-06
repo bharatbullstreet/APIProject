cddf389b4935f679b2c6627cc6e1fe8c0f87bd86
bharatbullstreet <bharatbullstreet@gmail.com>
2025-05-06 20:55:16 +0530

    check fourth commit

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -1,9 +1,5 @@
 const { join } = require('path');
 
-/**
- * @type {import("puppeteer").Configuration}
- */
 module.exports = {
-  // Puppeteer ke cache directory ka location define karein
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
